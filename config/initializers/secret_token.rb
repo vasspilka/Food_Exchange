16456db3c77dc8fa6743eaa4458bd747fc49a8f6
Vasilis Spilka <vasspilka@gmail.com>
2014-03-23 15:15:14 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodExchange::Application.config.secret_key_base = 'e5d147a00ca83aa1b5b00cdd498f62d4e1858cf640ccbe856dbb24932491b7931eb918e43c5ea8bd3f3470ae2ae23428d0a94d78c6d18cb8af10d6515f5645a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodExchange::Application.config.session_store :cookie_store, key: '_food_exchange_session'
