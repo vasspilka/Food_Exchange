74862d0353d1ea942b4f2ef1ef468c8dbc0314b5
Vasilis Spilka <vasspilka@gmail.com>
2014-04-02 21:13:44 +0300

    Donations to tab

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,16 +2,13 @@ class Donation < ActiveRecord::Base
   belongs_to :donator, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  has_many :items
   has_many :claims
   has_many :vindicators, through: :claims, source: :user
 
   geocoded_by :exchange_address  
   after_validation :geocode 
 
-  accepts_nested_attributes_for :items, allow_destroy: true
-
-  has_attached_file :image
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "01.png"
   validates_attachment :image,
   :content_type => { :content_type => ["image/jpeg","image/jpg", "image/gif", "image/png"] }
 
